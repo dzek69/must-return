95adb7865b7d8f98b9032d9ae478f0aefe4233fb
dzek <git-public@dzek.eu>
2017-01-22 23:53:49 +0100

    - Actual value has to be a function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const must = require("must");
 
 must.prototype.return = function() {
+    this.assert(typeof this.actual === "function", "be a", {
+        actual: typeof this.actual,
+        expected: "function",
+    });
 };
--- src/index.spec.js
+++ src/index.spec.js
@@ -5,9 +5,103 @@ const register = function() {
 };
 
 describe("must-return", function() {
-    it("registers a `return` method on `must`", function() {
-        (must.prototype.return === undefined).must.equal(true);
-        register();
-        must.prototype.return.must.exist();
+    describe("registers", function() {
+        it("a `return` method on `must`", function() {
+            (must.prototype.return === undefined).must.equal(true);
+            register();
+            must.prototype.return.must.be.a.function();
+        });
+    });
+
+    describe("when actual value is not a function", function() {
+        const stringAssertion = function() {
+            "string".must.return("hi there!");
+        };
+        const booleanAssertion = function() {
+            false.must.return("hi there!");
+        };
+        const NaNAssertion = function() {
+            NaN.must.return("hi there!");
+        };
+        const numberAssertion = function() {
+            (69.666).must.return("hi there!");
+        };
+        const objectAssertion = function() {
+            ({}).must.return("hi there!");
+        };
+        const undefinedAssertion = function() {
+            must(undefined).return("hi there!");
+        };
+
+        it("throws", function() {
+            stringAssertion.must.throw(must.AssertionError);
+            booleanAssertion.must.throw(must.AssertionError);
+            NaNAssertion.must.throw(must.AssertionError);
+            numberAssertion.must.throw(must.AssertionError);
+            objectAssertion.must.throw(must.AssertionError);
+            undefinedAssertion.must.throw(must.AssertionError);
+        });
+
+        it("throws meaningful information", function() {
+            try {
+                stringAssertion();
+            }
+            catch (e) {
+                const { message, actual, expected } = e;
+                actual.must.equal("string");
+                expected.must.equal("function");
+                message.must.equal("\"string\" must be a \"function\"");
+            }
+
+            try {
+                booleanAssertion();
+            }
+            catch (e) {
+                const { message, actual, expected } = e;
+                actual.must.equal("boolean");
+                expected.must.equal("function");
+                message.must.equal("false must be a \"function\"");
+            }
+
+            try {
+                NaNAssertion();
+            }
+            catch (e) {
+                const { message, actual, expected } = e;
+                actual.must.equal("number");
+                expected.must.equal("function");
+                message.must.equal("NaN must be a \"function\"");
+            }
+
+            try {
+                numberAssertion();
+            }
+            catch (e) {
+                const { message, actual, expected } = e;
+                actual.must.equal("number");
+                expected.must.equal("function");
+                message.must.equal("69.666 must be a \"function\"");
+            }
+
+            try {
+                objectAssertion();
+            }
+            catch (e) {
+                const { message, actual, expected } = e;
+                actual.must.equal("object");
+                expected.must.equal("function");
+                message.must.equal("{} must be a \"function\"");
+            }
+
+            try {
+                undefinedAssertion();
+            }
+            catch (e) {
+                const { message, actual, expected } = e;
+                actual.must.equal("undefined");
+                expected.must.equal("function");
+                message.must.equal("undefined must be a \"function\"");
+            }
+        });
     });
 });
